c0f7e6ce00bf1328c8f2268100531a880702e5ec
Alexander Bolhuis <alexander.bolhuis@gmail.com>
2019-05-12 22:27:42 +0200

    Level 2 and 3

--- first_app/urls.py
+++ first_app/urls.py
@@ -3,4 +3,5 @@ from first_app import views
 
 urlpatterns = [
     path('', views.first_app, name='welcome'),
+    path('forms', views.form_page, name='forms')
 ]
--- first_app/views.py
+++ first_app/views.py
@@ -1,11 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from first_app.models import Webpage, AccessRecord, Topic
+from first_app import forms
+
 
 # Create your views here.
 
+
 def index(request):
-    my_dict = {'insert_me':"This is a picture of an owl"}
-    return render(request, 'first_app/index.html', context=my_dict)
+    webpages_list = AccessRecord.objects.order_by('date')
+    date_dict = {'access_records': webpages_list}
+    return render(request, 'first_app/index.html', context=date_dict)
+
 
 def first_app(request):
     return HttpResponse("<h1>Welcome</h1>")
+
+
+def form_page(request):
+    form = forms.FormName()
+    if request.method == 'POST':
+        form = forms.FormName(request.POST)
+
+        if form.is_valid():
+            print("Validation success!")
+            print("NAME: " + form.cleaned_data['name'])
+            print("EMAIL: " + form.cleaned_data['email'])
+            print("TEXT: " + form.cleaned_data['text'])
+
+    return render(request, 'first_app/form_page.html', {'form': form})
